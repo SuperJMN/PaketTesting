d5bffee339d9b6a411f9d6c9f73966056cd253cc
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-12-19 17:45:26 +0100

    Broken System.Runtime version

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sample
+﻿namespace Sample
 {
+    using System;
+    using System.ComponentModel;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var f = new ClassLibrary.Class1();
         }
     }
 }
